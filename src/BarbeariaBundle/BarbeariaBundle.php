fb644f6f9204818c0e266f7eaa2a4806cf97fc56
Aluno <aluno@vm.elaborata.lab>
2017-03-23 20:18:29 -0300

    Commit inicial

---
+++ src/BarbeariaBundle/BarbeariaBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BarbeariaBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BarbeariaBundle extends Bundle
+{
+}
---
+++ src/BarbeariaBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace BarbeariaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('BarbeariaBundle:Default:index.html.twig');
+    }
+}
